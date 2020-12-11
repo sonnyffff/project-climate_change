c1f56f27c9cb2ce1760835cc907cca377f84d71c
sonnyffff <75819090+sonnyffff@users.noreply.github.com>
2020-12-11 19:38:30 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ This file contains code that runs the whole project.
 """
 from data_center import DataCenter
 from data_process import data_process_sealevel, data_process_temperature, generate_report
+from data_modeling import prediction_temperature, initialize_y_prime
 
 datacenter = DataCenter()
 data_process_temperature(datacenter, 'Vancouver.csv', 'climatedata.ca')
 data_process_sealevel(datacenter, 'Pacific Ocean.csv', 'noaa.gov')
-generate_report(datacenter)
+# generate_report(datacenter)
+# prediction_temperature(datacenter, 3)
+initialize_y_prime(datacenter, 5)
